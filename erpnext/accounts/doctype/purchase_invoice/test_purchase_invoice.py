46f814d2555a7b8d746ba88740ca349fc1edb33c
Deepesh Garg <deepeshgarg6@gmail.com>
2021-09-01 21:15:24 +0530

    test: Update test case
    (cherry picked from commit d5a736d11e5096f2861c3e5e6267584488df6d7d)

--- erpnext/accounts/doctype/purchase_invoice/test_purchase_invoice.py
+++ erpnext/accounts/doctype/purchase_invoice/test_purchase_invoice.py
@@ -1697,7 +1697,8 @@ def update_tax_witholding_category(company, account):
 		)
 =======
 	if not frappe.db.get_value('Tax Withholding Rate',
-		{'parent': 'TDS - 194 - Dividends - Individual', 'fiscal_year': fiscal_year[0]}):
+		{'parent': 'TDS - 194 - Dividends - Individual', 'from_date': ('>=', fiscal_year[1]),
+			'to_date': ('<=', fiscal_year[2])}):
 		tds_category = frappe.get_doc('Tax Withholding Category', 'TDS - 194 - Dividends - Individual')
 		tds_category.append('rates', {
 			'from_date': fiscal_year[1],
