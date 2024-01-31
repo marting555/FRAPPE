d5bad06f66518ce8a369571681779948b0933f60
jhon <jhonjtoloza@gmail.com>
2024-01-31 22:03:52 +0000

    button to pause move the queue

--- erpnext/projects/doctype/project/project_list.js
+++ erpnext/projects/doctype/project/project_list.js
@@ -7,8 +7,22 @@ frappe.listview_settings['Project'] = {
 		} else {
 			return [__(doc.status), frappe.utils.guess_colour(doc.status), "status,=," + doc.status];
 		}
+	},
+	before_render: async () => {
+		const page = frappe.pages['List/Project/List'].page;
+		const { auto_move_paused }  = await frappe.db.get_doc('Queue Settings')
+
+		page.add_menu_item(__(`Queue line move is ${auto_move_paused? 'Paused' : 'Runing'} <br> "Click to change"`), () => {		
+			frappe.confirm(__(`Please consfirm you want to <b>${auto_move_paused? 'Unpause': 'Pause'}</b> the queue auto move.`), () => {
+				frappe.db.set_value('Queue Settings', 'Queue Settings','auto_move_paused', auto_move_paused? 0 : 1)
+				location.reload()
+			})
+		})
 	}
+
+	
 };
+
 const el = document.createElement('erp-calendar')
 el.setAttribute('url', location.origin);
 const add = () => {
---
+++ erpnext/public/js/custom_buttons.js
@@ -0,0 +1,4 @@
+
+$(document).bind('DOMSubtreeModified', () => {
+  console.log(frappe.pages)
+})
