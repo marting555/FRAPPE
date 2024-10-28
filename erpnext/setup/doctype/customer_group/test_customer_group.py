2915dee81ea713a2e69d671ad35f0753102ef654
aartimanjare <aartimanjare@gmail.com>
2024-10-28 16:07:38 +0530

    feat: new test case test_customer_group_creation

--- erpnext/setup/doctype/customer_group/test_customer_group.py
+++ erpnext/setup/doctype/customer_group/test_customer_group.py
@@ -5,5 +5,23 @@ test_ignore = ["Price List"]
 
 
 import frappe
+import unittest
 
 test_records = frappe.get_test_records("Customer Group")
+
+class TestCustomerGroup(unittest.TestCase):
+    def setUp(self):
+        self.customer_group_name = "Test Customer Group 5"
+        self.customer_group = frappe.get_doc({
+            "doctype": "Customer Group",
+            "customer_group_name": self.customer_group_name,
+            "is_group": 0
+        })
+        self.customer_group.insert()
+
+    def test_customer_group_creation(self):
+        self.assertEqual(self.customer_group.customer_group_name, self.customer_group_name, "Customer Group name does not match.")
+        self.assertFalse(self.customer_group.is_group, "Customer Group should not be a group.")
+
+    def tearDown(self):
+        frappe.delete_doc("Customer Group", self.customer_group.name)
