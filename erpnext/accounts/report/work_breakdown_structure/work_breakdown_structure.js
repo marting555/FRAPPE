15da61e586475552987445a25d0518a55684b011
bansodegaurav8848 <gaurav@8848digital.com>
2024-11-08 17:29:53 +0530

    feat: create work breakdown structure report

---
+++ erpnext/accounts/report/work_breakdown_structure/work_breakdown_structure.js
@@ -0,0 +1,12 @@
+// Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Work Breakdown Structure"] = {
+	"filters": [
+
+	],
+	"tree": true,
+	"name_field": "parent",
+	"parent_field": "karigar",
+	"initial_depth": 0,
+};
