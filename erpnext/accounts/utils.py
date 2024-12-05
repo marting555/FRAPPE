e631fd1c1994d1097e7a2da8b4e15ca51185fbfa
Ashish <ashishverma8848>
2024-12-05 20:29:28 +0530

    fix: commints on erpnext on date 30 oct

--- erpnext/accounts/utils.py
+++ erpnext/accounts/utils.py
@@ -1992,6 +1992,7 @@ class QueryPaymentLedger:
 				ple.cost_center.as_("cost_center"),
 				Sum(ple.amount).as_("amount"),
 				Sum(ple.amount_in_account_currency).as_("amount_in_account_currency"),
+				ple.remarks,
 			)
 			.where(ple.delinked == 0)
 			.where(Criterion.all(filter_on_voucher_no))
@@ -2054,6 +2055,7 @@ class QueryPaymentLedger:
 				Table("vouchers").due_date,
 				Table("vouchers").currency,
 				Table("vouchers").cost_center.as_("cost_center"),
+				Table("vouchers").remarks,
 			)
 			.groupby(
 				Table("vouchers").account,
