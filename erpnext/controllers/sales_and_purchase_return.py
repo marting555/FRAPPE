bcf54b46aad8c2090bb3c0e0155779d66af16def
Ashish <ashishverma8848>
2024-12-18 18:46:30 +0530

    fix: erpnext commits on 10th dec

--- erpnext/controllers/sales_and_purchase_return.py
+++ erpnext/controllers/sales_and_purchase_return.py
@@ -25,6 +25,10 @@ def validate_return(doc):
 
 	if doc.return_against:
 		validate_return_against(doc)
+
+		if doc.doctype in ("Sales Invoice", "Purchase Invoice") and not doc.update_stock:
+			return
+		
 		if doc.doctype in ("Sales Invoice", "Purchase Invoice") and not doc.update_stock:
 			return
 		validate_returned_items(doc)
