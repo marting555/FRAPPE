5e7f3e1612d06cf4a582475d6ae264d1f6eb4b1b
UmakanthKaspa <kaspaumakanth1999@gmail.com>
2025-01-09 15:57:38 +0000

    fix: format code using prettier

--- erpnext/stock/report/stock_ageing/stock_ageing.js
+++ erpnext/stock/report/stock_ageing/stock_ageing.js
@@ -45,9 +45,9 @@ frappe.query_reports["Stock Ageing"] = {
 			fieldname: "item_code",
 			label: __("Item"),
 			fieldtype: "MultiSelectList",
-			get_data: function(txt) {
-				return frappe.db.get_link_options('Item', txt);
-			}
+			get_data: function (txt) {
+				return frappe.db.get_link_options("Item", txt);
+			},
 		},
 		{
 			fieldname: "brand",
