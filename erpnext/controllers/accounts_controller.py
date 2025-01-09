ef1c066ee5db2bb9a61627ba7b9c9e0efb83a0c1
barredterra <14891507+barredterra@users.noreply.github.com>
2025-01-09 16:20:44 +0100

    fix: run validations only if both dates are present

--- erpnext/controllers/accounts_controller.py
+++ erpnext/controllers/accounts_controller.py
@@ -2181,7 +2181,9 @@ class AccountsController(TransactionBase):
 			return
 
 		for d in self.get("payment_schedule"):
-			d.validate_from_to_dates("discount_date", "due_date")
+			if d.due_date and d.discount_date:
+				d.validate_from_to_dates("discount_date", "due_date")
+
 			if self.doctype == "Sales Order" and getdate(d.due_date) < getdate(self.transaction_date):
 				frappe.throw(
 					_("Row {0}: Due Date in the Payment Terms table cannot be before Posting Date").format(
