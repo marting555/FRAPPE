33ef29e278f3bfcc5b5e5a2fcdbe44a1ac7f8a65
Barathpalanisamy <barath@8848digital.com>
2025-01-10 18:20:04 +0530

    test: pos opening and closing entry with opening amount - TC_S_101

--- erpnext/accounts/doctype/pos_opening_entry/test_pos_opening_entry.py
+++ erpnext/accounts/doctype/pos_opening_entry/test_pos_opening_entry.py
@@ -53,6 +53,42 @@ class TestPOSOpeningEntry(unittest.TestCase):
 		closing_enrty.submit()
 		opening_entry.reload()
 		self.assertEqual(opening_entry.status, "Closed")
+	
+	def test_pos_opening_to_closing_enrty_with_opening_balance_TC_S_101(self):
+		from erpnext.accounts.doctype.pos_closing_entry.test_pos_closing_entry import init_user_and_profile		
+		test_user, pos_profile = init_user_and_profile()
+
+		opening_entry = frappe.new_doc("POS Opening Entry")
+		opening_entry.pos_profile = pos_profile.name
+		opening_entry.user = test_user.name
+		opening_entry.company = pos_profile.company
+		opening_entry.period_start_date = frappe.utils.get_datetime()
+
+		# Add opening balance details
+		balance_details = []
+		for d in pos_profile.payments:
+			balance_details.append({
+				"mode_of_payment": d.mode_of_payment,
+				"opening_amount": 1000
+			})
+
+		opening_entry.set("balance_details", balance_details)
+		opening_entry.save()
+		opening_entry.submit()
+		self.assertEqual(opening_entry.status, "Open")
+		
+		pos_inv1 = create_pos_invoice(rate=3500, do_not_submit=1)
+		pos_inv1.append("payments", {"mode_of_payment": "Cash", "account": "Cash - _TC", "amount": 3500})
+		pos_inv1.submit()
+
+		pos_inv2 = create_pos_invoice(rate=3200, do_not_submit=1)
+		pos_inv2.append("payments", {"mode_of_payment": "Cash", "account": "Cash - _TC", "amount": 3200})
+		pos_inv2.submit()
+
+		closing_enrty= make_closing_entry_from_opening(opening_entry)
+		closing_enrty.submit()
+		opening_entry.reload()
+		self.assertEqual(opening_entry.status, "Closed")
 
 def create_opening_entry(pos_profile, user):
 	entry = frappe.new_doc("POS Opening Entry")
