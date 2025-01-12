3df93beddb0f4553273850f5af91687f317dd1ea
IamSaiyyamChhetri <IamSaiyyamChhetri>
2025-01-13 01:29:25 +0530

    test: PR to PI with Additional discount on Grand Total

--- erpnext/stock/doctype/purchase_receipt/test_purchase_receipt.py
+++ erpnext/stock/doctype/purchase_receipt/test_purchase_receipt.py
@@ -4207,6 +4207,58 @@ class TestPurchaseReceipt(FrappeTestCase):
 		pi_total = sum(entry["debit"] for entry in pi_gl_entries)
 		self.assertEqual(pi_total, 10080) 
 
+	def test_pr_with_additional_discount_TC_B_059(self):
+		# Scenario : PR => PI [With Additional Discount]
+		from erpnext.stock.doctype.purchase_receipt.purchase_receipt import (
+			make_purchase_invoice as make_pi_from_pr,
+		)
+
+		pr_data = {
+			"company" : "_Test Company",
+			"item_code" : "_Test Item",
+			"warehouse" : "Stores - _TC",
+			"supplier": "_Test Supplier",
+            "schedule_date": "2025-01-13",
+			"qty" : 1,
+			"rate" : 10000,
+			"apply_discount_on" : "Grand Total",
+			"additional_discount_percentage" :10 ,
+			"do_not_submit":1
+		}
+
+		acc = frappe.new_doc("Account")
+		acc.account_name = "Input Tax IGST"
+		acc.parent_account = "Tax Assets - _TC"
+		acc.company = "_Test Company"
+		account_name = frappe.db.exists("Account", {"account_name" : "Input Tax IGST","company": "_Test Company" })
+		if not account_name:
+			account_name = acc.insert()
+
+		doc_pr = make_purchase_receipt(**pr_data)
+		doc_pr.append("taxes", {
+                    "charge_type": "On Net Total",
+                    "account_head": account_name,
+                    "rate": 12,
+                    "description": "Input GST",
+                })
+		doc_pr.submit()
+		self.assertEqual(doc_pr.discount_amount, 1120)
+		self.assertEqual(doc_pr.grand_total, 10080)
+
+		pi = make_pi_from_pr(doc_pr.name)
+		pi.insert()
+		pi.submit()
+
+		self.assertEqual(pi.discount_amount, 1120)
+		self.assertEqual(pi.grand_total, 10080)
+
+		# Accounting Ledger Checks
+		pi_gl_entries = frappe.get_all("GL Entry", filters={"voucher_no": pi.name}, fields=["account", "debit", "credit"])
+
+		# PI Ledger Validation
+		pi_total = sum(entry["debit"] for entry in pi_gl_entries)
+		self.assertEqual(pi_total, 10080) 
+
 def prepare_data_for_internal_transfer():
 	from erpnext.accounts.doctype.sales_invoice.test_sales_invoice import create_internal_supplier
 	from erpnext.selling.doctype.customer.test_customer import create_internal_customer
@@ -4382,7 +4434,7 @@ def make_purchase_receipt(**args):
 	pr.apply_putaway_rule = args.apply_putaway_rule
 	pr.apply_discount_on = args.apply_discount_on or None	
 	pr.additional_discount_percentage = args.additional_discount_percentage or None	
-	
+
 	qty = args.qty or 5
 	rejected_qty = args.rejected_qty or 0
 	received_qty = args.received_qty or flt(rejected_qty) + flt(qty)
