523114487cc1a89d96320f0ef677efba4830bcf9
hrishikesh <hrishikesh@8848digital.com>
2025-01-13 10:13:38 +0530

    feat:custom_bank_reconciliation_tool

---
+++ erpnext/accounts/doctype/erp_transaction/erp_transaction.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ERPTransaction(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		date: DF.Date | None
+		deposit: DF.Currency
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+		reference_doc: DF.Literal["Payment Entry", "Journal Entry"]
+		reference_id: DF.Data | None
+		reference_number: DF.Data | None
+		remaining_amount: DF.Currency
+		withdraw: DF.Currency
+	# end: auto-generated types
+	pass
