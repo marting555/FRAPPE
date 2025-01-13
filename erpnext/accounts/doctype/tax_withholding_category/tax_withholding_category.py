6b1fa7a20876b213724d8cf7dc10e0bab1b53c6c
venkat102 <venkatesharunachalam659@gmail.com>
2025-01-13 14:23:43 +0530

    fix: get invoice without apply_tds when Consider Entire Party Ledger Amount is enabled

--- erpnext/accounts/doctype/tax_withholding_category/tax_withholding_category.py
+++ erpnext/accounts/doctype/tax_withholding_category/tax_withholding_category.py
@@ -352,7 +352,7 @@ def get_invoice_vouchers(parties, tax_details, company, party_type="Supplier"):
 		"docstatus": 1,
 	}
 
-	if doctype != "Sales Invoice":
+	if doctype != "Sales Invoice" and not cint(tax_details.consider_party_ledger_amount):
 		filters.update(
 			{"apply_tds": 1, "tax_withholding_category": tax_details.get("tax_withholding_category")}
 		)
