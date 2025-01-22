c9213265178e2b682002325e29e1370841e87409
vinod-8848digital <vinod@8848digital.com>
2025-01-23 00:37:24 +0530

    test: TC_B_116 test case for shipping rule with net total and restricted to country then in the purchase order error should raise

--- erpnext/buying/doctype/purchase_order/test_purchase_order.py
+++ erpnext/buying/doctype/purchase_order/test_purchase_order.py
@@ -5269,6 +5269,63 @@ class TestPurchaseOrder(FrappeTestCase):
 			po.insert()
 		self.assertEqual(str(cm.exception), "Shipping rule not applicable for country India in Shipping Address")
 
+	def test_shipping_rule_net_total_restricted_country_po_with_gst_TC_B_116(self):
+		company = "_Test Company"
+		warehouse = "Stores - _TC"
+		supplier = "_Test Supplier 1"
+		item_code = "test_item_with_shipping_rule"
+		gst_hsn_code = "888890"
+
+		if not frappe.db.exists("Item", item_code):
+			frappe.get_doc({
+				"doctype": "Item",
+				"item_code": item_code,
+				"item_name": item_code,
+				"gst_hsn_code": gst_hsn_code,
+				"is_stock_item": 1
+			}).insert()
+
+		shipping_rule = frappe.get_doc({
+			"doctype": "Shipping Rule",
+			"company": company,
+			"label": "test_shipping_rule_restricted_country",
+			"calculate_based_on": "Net Total",
+			"shipping_rule_type": "Buying",
+			"account": "Creditors - _TC",
+			"cost_center": "Main - _TC",
+			"conditions": [{
+				"from_value": 500,
+				"to_value": 2000,
+				"shipping_amount": 500
+			}],
+			"countries":[
+				{
+					"country": "Australia"
+				}
+			]
+		}).insert()
+
+		po = frappe.get_doc({
+			"doctype": "Purchase Order",
+			"supplier": supplier,
+			"company": company,
+			"schedule_date": today(),
+			"set_warehouse": warehouse,
+			"items": [
+				{
+					"item_code": item_code,
+					"qty": 10,
+					"rate": 100,
+					"warehouse": warehouse,
+				}
+			],
+			"taxes_and_charges": "Input GST In-state - _TC",
+			"shipping_rule": shipping_rule.name
+		})
+		with self.assertRaises(frappe.exceptions.ValidationError) as cm:
+			po.insert()
+		self.assertEqual(str(cm.exception), "Shipping rule not applicable for country India in Shipping Address")
+
 def create_po_for_sc_testing():
 	from erpnext.controllers.tests.test_subcontracting_controller import (
 		make_bom_for_subcontracted_items,
