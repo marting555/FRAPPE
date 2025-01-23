99406979404dfa1c8e7478e4cf3082ab7b105d1a
anandk-spyke-o1 <anandkumar@8848digital.com>
2025-01-23 22:36:48 +0530

    test: Added test case for monthly deferred revenue TC_ACC_093

--- erpnext/accounts/doctype/process_deferred_accounting/test_process_deferred_accounting.py
+++ erpnext/accounts/doctype/process_deferred_accounting/test_process_deferred_accounting.py
@@ -159,6 +159,57 @@ class TestProcessDeferredAccounting(unittest.TestCase):
 
 		change_acc_settings()
 
+	def test_auto_deferred_revenue_TC_ACC_093(self):
+		"""Test auto deffered revenue on monthly basis."""
+		change_acc_settings(acc_frozen_upto="2023-05-31", book_deferred_entries_based_on="Months")
+
+		deferred_account = create_account(
+			account_name="Deferred Revenue for Accounts Frozen",
+			parent_account="Current Liabilities - _TC",
+			company="_Test Company",
+		)
+
+		item = create_item("_Test Item for Deferred Accounting")
+		item.enable_deferred_revenue = 1
+		item.deferred_revenue_account = deferred_account
+		item.no_of_months = 12
+		item.save()
+
+		si = create_sales_invoice(
+			item=item.name, rate=3000, update_stock=0, posting_date="2023-07-01", do_not_submit=True
+		)
+		si.items[0].enable_deferred_revenue = 1
+		si.items[0].service_start_date = "2023-05-01"
+		si.items[0].service_end_date = "2023-07-31"
+		si.items[0].deferred_revenue_account = deferred_account
+		si.save()
+		si.submit()
+
+		process_deferred_accounting = frappe.get_doc(
+			dict(
+				doctype="Process Deferred Accounting",
+				posting_date="2023-07-01",
+				start_date="2023-05-01",
+				end_date="2023-06-30",
+				type="Income",
+			)
+		)
+
+		process_deferred_accounting.insert()
+		process_deferred_accounting.submit()
+
+		expected_gle = [
+			["Debtors - _TC", 3000, 0.0, "2023-07-01"],
+			[deferred_account, 0.0, 3000, "2023-07-01"],
+			["Sales - _TC", 0.0, 1000, "2023-06-30"],
+			[deferred_account, 1000, 0.0, "2023-06-30"],
+			["Sales - _TC", 0.0, 1000, "2023-06-30"],
+			[deferred_account, 1000, 0.0, "2023-06-30"],
+		]
+
+		check_gl_entries(self, si.name, expected_gle, "2023-07-01")
+		change_acc_settings()
+
 
 	def test_pda_submission_and_cancellation(self):
 		pda = frappe.get_doc(
