cec0c650aeceae92b9f79c78dadb8dece35ef1b3
bvisible <info@bvisible.ch>
2025-02-10 09:36:29 +0100

    fix: add float precision handling in loyalty program points calculation
    - Add system float precision configuration for loyalty amount calculation
    - Fix precision issues with loyalty points conversion (e.g., 290 points with 0.1 conversion factor = 29.90)
    - Use system default float precision or fallback to 2 decimal places

--- erpnext/accounts/doctype/loyalty_program/loyalty_program.py
+++ erpnext/accounts/doctype/loyalty_program/loyalty_program.py
@@ -1,12 +1,10 @@
 # Copyright (c) 2018, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-
 import frappe
 from frappe import _
 from frappe.model.document import Document
-from frappe.utils import flt, today
-
+from frappe.utils import flt, today, cint
 
 class LoyaltyProgram(Document):
 	# begin: auto-generated types
@@ -166,7 +164,8 @@ def validate_loyalty_points(ref_doc, points_to_redeem):
 		if points_to_redeem > loyalty_program_details.loyalty_points:
 			frappe.throw(_("You don't have enough Loyalty Points to redeem"))
 
-		loyalty_amount = flt(points_to_redeem * loyalty_program_details.conversion_factor)
+		float_precision = cint(frappe.db.get_default("float_precision")) or 2
+		loyalty_amount = flt(points_to_redeem * loyalty_program_details.conversion_factor, float_precision)
 
 		if loyalty_amount > ref_doc.rounded_total:
 			frappe.throw(_("You can't redeem Loyalty Points having more value than the Rounded Total."))
