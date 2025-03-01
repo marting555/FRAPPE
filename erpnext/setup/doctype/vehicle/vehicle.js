ed0630c81c74ad2edaa5322acefbc401d3839285
Dinie Masjuki <diniemasjuki@users.noreply.github.com>
2025-03-01 11:30:15 +0800

    Update vehicle.js

--- erpnext/setup/doctype/vehicle/vehicle.js
+++ erpnext/setup/doctype/vehicle/vehicle.js
@@ -2,5 +2,15 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Vehicle", {
-	refresh: function (frm) {},
+	refresh: function (frm) {
+
+		// Make the field editable if the value is 0
+        if (frm.doc.last_odometer == 0) {
+            frm.fields_dict['last_odometer'].df.read_only = 0;  // Make it editable
+        } else {
+            frm.fields_dict['last_odometer'].df.read_only = 1;  // Make it read-only
+        }        
+        frm.refresh_field('last_odometer'); // Refresh the field to apply the changes
+		
+	},
 });
