4f9bb07d13e721bcd3b42797ef754434808db0f0
Nabin Hait <nabinhait@gmail.com>
2025-03-06 17:37:37 +0530

    chore: linter issues

--- erpnext/public/js/utils/contact_address_quick_entry.js
+++ erpnext/public/js/utils/contact_address_quick_entry.js
@@ -16,13 +16,13 @@ frappe.ui.form.ContactAddressQuickEntryForm = class ContactAddressQuickEntryForm
 	insert() {
 		/**
 		 * Using alias fieldnames because the doctype definition define "email_id" and "mobile_no" as readonly fields.
-		 * Therefor, resulting in the fields being "hidden".
+		 * This results in the fields being "hidden".
 		 */
 		const map_field_names = {
 			email_address: "email_id",
 			mobile_number: "mobile_no",
 			map_to_first_name : "first_name",
-			map_to_last_name: "last_name"
+			map_to_last_name: "last_name",
 		};
 
 		Object.entries(map_field_names).forEach(([fieldname, new_fieldname]) => {
@@ -44,13 +44,13 @@ frappe.ui.form.ContactAddressQuickEntryForm = class ContactAddressQuickEntryForm
 				label: __("First Name"),
 				fieldname: "map_to_first_name",
 				fieldtype: "Data",
-				depends_on: "eval:doc.customer_type=='Company'"
+				depends_on: "eval:doc.customer_type=='Company'",
 			},
 			{
 				label: __("Last Name"),
 				fieldname: "map_to_last_name",
 				fieldtype: "Data",
-				depends_on: "eval:doc.customer_type=='Company'"
+				depends_on: "eval:doc.customer_type=='Company'",
 			},
 
 			{
