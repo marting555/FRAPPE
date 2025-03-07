b8bc581b369f3bae3cfd2eb5e1a846362fcecb95
hrishikesh8848 <hrishikesh@8848digital.com>
2025-03-07 08:42:13 +0530

    fix: codecoverage TC_SCK_162

--- erpnext/stock/doctype/quality_inspection/test_quality_inspection.py
+++ erpnext/stock/doctype/quality_inspection/test_quality_inspection.py
@@ -358,8 +358,9 @@ class TestQualityInspection(FrappeTestCase):
 		si.cancel()
 
 	def test_qa_for_pr_out_TC_SCK_162(self):
+		create_company()
 		item_code = create_item("_Test Item with QA", valuation_rate=200).name
-		pr = make_purchase_receipt(item_code = item_code)
+		pr = make_purchase_receipt(item_code = item_code, company = "_Test Company QA")
 		frappe.db.set_value("Item", "_Test Item with QA", "inspection_required_before_purchase", 1)
 
 		qa = create_quality_inspection(
