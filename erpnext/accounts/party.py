7587c31fda21aa1a4543fdf5c662fc65c6ff221e
Abdeali Chharchhoda <abdealiking786@gmail.com>
2025-03-25 11:45:53 +0530

    fix: enhance contact details handling for Employee party type in payment entry

--- erpnext/accounts/party.py
+++ erpnext/accounts/party.py
@@ -280,9 +280,26 @@ def get_regional_address_details(party_details, doctype, company):
 
 
 def complete_contact_details(party_details):
-	if not party_details.contact_person:
-		party_details.update(
-			{
+	contact_details = frappe._dict()
+
+	if party_details.party_type == "Employee":
+		contact_details = frappe.db.get_value(
+			"Employee",
+			party_details.party,
+			[
+				"employee_name as contact_display",
+				"prefered_email as contact_email",
+				"cell_number as contact_mobile",
+				"designation as contact_designation",
+				"department as contact_department",
+			],
+			as_dict=True,
+		)
+
+		contact_details.update({"contact_person": None, "contact_phone": None})
+	else:
+		if not party_details.contact_person:
+			contact_details = {
 				"contact_person": None,
 				"contact_display": None,
 				"contact_email": None,
@@ -291,21 +308,23 @@ def complete_contact_details(party_details):
 				"contact_designation": None,
 				"contact_department": None,
 			}
-		)
-	else:
-		fields = [
-			"name as contact_person",
-			"full_name as contact_display",
-			"email_id as contact_email",
-			"mobile_no as contact_mobile",
-			"phone as contact_phone",
-			"designation as contact_designation",
-			"department as contact_department",
-		]
+		else:
+			contact_details = frappe.db.get_value(
+				"Contact",
+				party_details.contact_person,
+				[
+					"name as contact_person",
+					"full_name as contact_display",
+					"email_id as contact_email",
+					"mobile_no as contact_mobile",
+					"phone as contact_phone",
+					"designation as contact_designation",
+					"department as contact_department",
+				],
+				as_dict=True,
+			)
 
-		contact_details = frappe.db.get_value("Contact", party_details.contact_person, fields, as_dict=True)
-
-		party_details.update(contact_details)
+	party_details.update(contact_details)
 
 
 def set_contact_details(party_details, party, party_type):
