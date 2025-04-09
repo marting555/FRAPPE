8c9aa428b00f5e30c10ee8b72b3410bb2be6558a
vinod-8848digital <vinod@8848digital.com>
2025-04-10 01:02:18 +0530

    chore: fix conflicts

--- erpnext/manufacturing/doctype/bom/bom.py
+++ erpnext/manufacturing/doctype/bom/bom.py
@@ -1508,12 +1508,6 @@ def item_query(doctype, txt, searchfield, start, page_len, filters):
 	fields = ["name", "item_name", "item_group", "description"]
 	fields.extend([field for field in searchfields if field not in ["name", "item_group", "description"]])
 
-	searchfields = searchfields + [
-		field
-		for field in [searchfield or "name", "item_code", "item_group", "item_name"]
-		if field not in searchfields
-	]
-
 	if not searchfields:
 		searchfields = ["name"]
 
