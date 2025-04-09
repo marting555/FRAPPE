241b8607387acdeb14e5d23f7a09cc2fc0922492
vinod-8848digital <vinod@8848digital.com>
2025-04-09 16:10:18 +0530

    fix: make message translatable

--- erpnext/public/js/utils/unreconcile.js
+++ erpnext/public/js/utils/unreconcile.js
@@ -240,7 +240,7 @@ erpnext.accounts.unreconcile_payment = {
 									);
 									d.hide();
 								} else {
-									frappe.msgprint("No Selection");
+									frappe.msgprint(__("No Selection"));
 								}
 							},
 						});
