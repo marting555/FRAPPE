c99ba24c16f4df69bf414f7e875d4d6c5a6f222f
Hung Tran <hungtranthanh0302@gmail.com>
2025-05-08 14:17:17 +0700

    erpnext/selling/doctype/promotion/promotion.json type option to be English

--- erpnext/selling/doctype/promotion/promotion.py
+++ erpnext/selling/doctype/promotion/promotion.py
@@ -18,6 +18,6 @@ class Promotion(Document):
 		priority: DF.Literal["", "G0", "G1", "G2", "G3", "G4", "G5", "G6", "G7"]
 		scope: DF.Literal["Line Item", "Order"]
 		title: DF.Data
-		type: DF.Literal["Gi\u1ea3m theo ph\u1ea7n tr\u0103m", "Gi\u1ea3m theo s\u1ed1 ti\u1ec1n", "Qu\u00e0 t\u1eb7ng"]
+		type: DF.Literal["Percentage", "Fix Amount", "Gift"]
 	# end: auto-generated types
 	pass
