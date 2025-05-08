7790f4445196dd31c709b549cd9ca9b181405c1b
Hung Tran <hungtranthanh0302@gmail.com>
2025-05-08 11:15:22 +0700

    remove Vietnamese harcode

--- erpnext/selling/doctype/promotion/promotion.py
+++ erpnext/selling/doctype/promotion/promotion.py
@@ -16,7 +16,7 @@ class Promotion(Document):
 
 		is_expired: DF.Check
 		priority: DF.Literal["", "G0", "G1", "G2", "G3", "G4", "G5", "G6", "G7"]
-		scope: DF.Literal["S\u1ea3n ph\u1ea9m", "\u0110\u01a1n h\u00e0ng"]
+		scope: DF.Literal["Line Item", "Order"]
 		title: DF.Data
 		type: DF.Literal["Gi\u1ea3m theo ph\u1ea7n tr\u0103m", "Gi\u1ea3m theo s\u1ed1 ti\u1ec1n", "Qu\u00e0 t\u1eb7ng"]
 	# end: auto-generated types
