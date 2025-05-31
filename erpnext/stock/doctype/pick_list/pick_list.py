47b83e513bd6e32ebd73700ec3f5d8410bbe9e4a
Smit Vora <smitvora203@gmail.com>
2025-05-31 17:12:49 +0530

    fix: correct UOM conversion in create DN from picklist

--- erpnext/stock/doctype/pick_list/pick_list.py
+++ erpnext/stock/doctype/pick_list/pick_list.py
@@ -1254,7 +1254,7 @@ def map_pl_locations(pick_list, item_mapper, delivery_note, sales_order=None):
 		if dn_item:
 			dn_item.pick_list_item = location.name
 			dn_item.warehouse = location.warehouse
-			dn_item.qty = flt(location.picked_qty) / (flt(location.conversion_factor) or 1)
+			dn_item.qty = flt(location.picked_qty) / (flt(dn_item.conversion_factor) or 1)
 			dn_item.batch_no = location.batch_no
 			dn_item.serial_no = location.serial_no
 			dn_item.use_serial_batch_fields = location.use_serial_batch_fields
