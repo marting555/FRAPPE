aebfc46f092d074a9998d721e0c86831224f74f3
JEMMIA <jemmia@MacBook-Air-cua-JEMMIA.local>
2025-06-09 13:37:14 +0700

    fix: ai hub url

--- erpnext/packages/ai_hub/ai_hub.py
+++ erpnext/packages/ai_hub/ai_hub.py
@@ -19,7 +19,7 @@ class AIHubClient(AIHubConnector):
         data = {
             "conversationId" : conversation_id,
             "pageId": page_id,
-            "webhookUrl": f"{self.webhook_url}/webhook/erp/leads/info"
+            "webhookUrl": f"{self.webhook_url}/webhook/ai-hub/erp/leads"
         }
 
         print(data)
