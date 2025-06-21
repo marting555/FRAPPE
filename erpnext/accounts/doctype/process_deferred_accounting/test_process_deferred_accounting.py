893126bb92a880a4ac2db9b28375e140164f1ef9
ruthra kumar <ruthra@erpnext.com>
2025-06-21 16:26:36 +0530

    refactor(test): fix flaky process deferred accounting tests

--- erpnext/accounts/doctype/process_deferred_accounting/test_process_deferred_accounting.py
+++ erpnext/accounts/doctype/process_deferred_accounting/test_process_deferred_accounting.py
@@ -47,6 +47,7 @@ class TestProcessDeferredAccounting(ERPNextTestSuite):
 				start_date="2023-05-01",
 				end_date="2023-06-30",
 				type="Income",
+				company=self.companies[0].name,
 			)
 		)
 
@@ -73,6 +74,7 @@ class TestProcessDeferredAccounting(ERPNextTestSuite):
 				start_date="2019-01-01",
 				end_date="2019-01-31",
 				type="Income",
+				company=self.companies[0].name,
 			)
 		)
 		pda.submit()
