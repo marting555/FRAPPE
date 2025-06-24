f8929bc19ed093c71b5083453ad078b36f0527aa
Rohit Waghchaure <rohitw1991@gmail.com>
2025-06-24 14:31:58 +0530

    fix: space

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -55,6 +55,7 @@ setup_wizard_requires = "assets/erpnext/js/setup_wizard.js"
 setup_wizard_stages = "erpnext.setup.setup_wizard.setup_wizard.get_setup_stages"
 setup_wizard_complete = "erpnext.setup.setup_wizard.setup_wizard.setup_demo"
 setup_wizard_test = "erpnext.setup.setup_wizard.test_setup_wizard.run_setup_wizard_test"
+
 before_install = [
 	"erpnext.setup.install.check_frappe_version",
 ]
