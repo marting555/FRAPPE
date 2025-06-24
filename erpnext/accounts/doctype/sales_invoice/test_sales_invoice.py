0790db79fdf8a4510e651d643f8eceaefc1553de
Lakshit Jain <108322669+ljain112@users.noreply.github.com>
2025-06-24 15:40:00 +0530

    fix: get already billed amount from current doc instead of database (#48079)
    * fix: get already billed amount from current doc instead of database
    
    * fix: throw overbilling validation for all items in single call
    
    * refactor: minor fixes
    
    ---------
    
    Co-authored-by: Sagar Vora <16315650+sagarvora@users.noreply.github.com>
    (cherry picked from commit 47c3c4808e78ec29678c4c1972fc8cbd387cb1c3)
    
    # Conflicts:
    #	erpnext/accounts/doctype/sales_invoice/test_sales_invoice.py

--- erpnext/accounts/doctype/sales_invoice/test_sales_invoice.py
+++ erpnext/accounts/doctype/sales_invoice/test_sales_invoice.py
@@ -3287,6 +3287,7 @@ class TestSalesInvoice(FrappeTestCase):
 		si.posting_date = getdate()
 		si.submit()
 
+	@IntegrationTestCase.change_settings("Accounts Settings", {"over_billing_allowance": 0})
 	def test_over_billing_case_against_delivery_note(self):
 		"""
 		Test a case where duplicating the item with qty = 1 in the invoice
@@ -3294,24 +3295,34 @@ class TestSalesInvoice(FrappeTestCase):
 		"""
 		from erpnext.stock.doctype.delivery_note.test_delivery_note import create_delivery_note
 
+<<<<<<< HEAD
 		over_billing_allowance = frappe.db.get_single_value("Accounts Settings", "over_billing_allowance")
 		frappe.db.set_value("Accounts Settings", None, "over_billing_allowance", 0)
 
+=======
+>>>>>>> 47c3c4808e (fix: get already billed amount from current doc instead of database (#48079))
 		dn = create_delivery_note()
 		dn.submit()
 
 		si = make_sales_invoice(dn.name)
-		# make a copy of first item and add it to invoice
 		item_copy = frappe.copy_doc(si.items[0])
+		si.save()
+
+		si.items = []  # Clear existing items
 		si.append("items", item_copy)
 		si.save()
 
+		si.append("items", item_copy)
 		with self.assertRaises(frappe.ValidationError) as err:
-			si.submit()
+			si.save()
 
 		self.assertTrue("cannot overbill" in str(err.exception).lower())
+<<<<<<< HEAD
 
 		frappe.db.set_value("Accounts Settings", None, "over_billing_allowance", over_billing_allowance)
+=======
+		dn.cancel()
+>>>>>>> 47c3c4808e (fix: get already billed amount from current doc instead of database (#48079))
 
 	@change_settings(
 		"Accounts Settings",
